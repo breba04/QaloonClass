30ed969b0fca979db6ef4a12e84e17c7d30228da
Breba Hamid <ibrahimabdo407@gmail.com>
2026-04-04 19:22:44 +0200

    Add Based Card Design

--- UI/BasedUserControl/ctrlBaseCard.Designer.cs
+++ UI/BasedUserControl/ctrlBaseCard.Designer.cs
@@ -30,17 +30,37 @@
         {
             this.pnTop = new System.Windows.Forms.Panel();
             this.pnBaseInfo = new System.Windows.Forms.Panel();
+            this.lbID = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.btnIsActive = new System.Windows.Forms.Button();
             this.pnLine = new System.Windows.Forms.Panel();
             this.pnFullName = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.lbID = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.pnContecnt = new System.Windows.Forms.Panel();
+            this.pnLeftContent = new System.Windows.Forms.Panel();
+            this.pnRightContent = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pnButtom = new System.Windows.Forms.Panel();
+            this.pnButtomContentRight = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
             this.pnTop.SuspendLayout();
             this.pnBaseInfo.SuspendLayout();
             this.pnFullName.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnContecnt.SuspendLayout();
+            this.pnLeftContent.SuspendLayout();
+            this.pnRightContent.SuspendLayout();
+            this.pnButtom.SuspendLayout();
+            this.pnButtomContentRight.SuspendLayout();
+            this.panel6.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnTop
@@ -53,7 +73,7 @@
             this.pnTop.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnTop.Location = new System.Drawing.Point(11, 11);
             this.pnTop.Name = "pnTop";
-            this.pnTop.Size = new System.Drawing.Size(868, 152);
+            this.pnTop.Size = new System.Drawing.Size(884, 152);
             this.pnTop.TabIndex = 0;
             // 
             // pnBaseInfo
@@ -66,12 +86,40 @@
             this.pnBaseInfo.Location = new System.Drawing.Point(184, 72);
             this.pnBaseInfo.Name = "pnBaseInfo";
             this.pnBaseInfo.Padding = new System.Windows.Forms.Padding(20);
-            this.pnBaseInfo.Size = new System.Drawing.Size(684, 80);
+            this.pnBaseInfo.Size = new System.Drawing.Size(700, 80);
             this.pnBaseInfo.TabIndex = 5;
+            // 
+            // lbID
+            // 
+            this.lbID.AutoSize = true;
+            this.lbID.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lbID.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(65)))), ((int)(((byte)(38)))));
+            this.lbID.Location = new System.Drawing.Point(569, 20);
+            this.lbID.Margin = new System.Windows.Forms.Padding(50, 0, 5, 0);
+            this.lbID.Name = "lbID";
+            this.lbID.Size = new System.Drawing.Size(32, 28);
+            this.lbID.TabIndex = 8;
+            this.lbID.Text = "؟؟";
+            this.lbID.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(65)))), ((int)(((byte)(38)))));
+            this.label2.Location = new System.Drawing.Point(601, 20);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 28);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "المعرف:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // btnIsActive
             // 
-            this.btnIsActive.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(130)))), ((int)(((byte)(57)))));
+            this.btnIsActive.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(138)))), ((int)(((byte)(67)))));
             this.btnIsActive.Dock = System.Windows.Forms.DockStyle.Left;
             this.btnIsActive.FlatAppearance.BorderSize = 0;
             this.btnIsActive.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
@@ -90,7 +138,7 @@
             this.pnLine.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnLine.Location = new System.Drawing.Point(184, 71);
             this.pnLine.Name = "pnLine";
-            this.pnLine.Size = new System.Drawing.Size(684, 1);
+            this.pnLine.Size = new System.Drawing.Size(700, 1);
             this.pnLine.TabIndex = 4;
             // 
             // pnFullName
@@ -100,7 +148,7 @@
             this.pnFullName.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnFullName.Location = new System.Drawing.Point(184, 0);
             this.pnFullName.Name = "pnFullName";
-            this.pnFullName.Size = new System.Drawing.Size(684, 71);
+            this.pnFullName.Size = new System.Drawing.Size(700, 71);
             this.pnFullName.TabIndex = 3;
             // 
             // label1
@@ -128,51 +176,167 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // lbID
+            // pnContecnt
             // 
-            this.lbID.AutoSize = true;
-            this.lbID.Dock = System.Windows.Forms.DockStyle.Right;
-            this.lbID.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(65)))), ((int)(((byte)(38)))));
-            this.lbID.Location = new System.Drawing.Point(553, 20);
-            this.lbID.Margin = new System.Windows.Forms.Padding(50, 0, 5, 0);
-            this.lbID.Name = "lbID";
-            this.lbID.Size = new System.Drawing.Size(32, 28);
-            this.lbID.TabIndex = 8;
-            this.lbID.Text = "؟؟";
-            this.lbID.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.pnContecnt.Controls.Add(this.pnRightContent);
+            this.pnContecnt.Controls.Add(this.pnLeftContent);
+            this.pnContecnt.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnContecnt.Location = new System.Drawing.Point(11, 163);
+            this.pnContecnt.Name = "pnContecnt";
+            this.pnContecnt.Padding = new System.Windows.Forms.Padding(50);
+            this.pnContecnt.Size = new System.Drawing.Size(884, 300);
+            this.pnContecnt.TabIndex = 1;
             // 
-            // label2
+            // pnLeftContent
             // 
-            this.label2.AutoSize = true;
-            this.label2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(65)))), ((int)(((byte)(38)))));
-            this.label2.Location = new System.Drawing.Point(585, 20);
-            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 28);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "المعرف:";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.pnLeftContent.Controls.Add(this.panel2);
+            this.pnLeftContent.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnLeftContent.Location = new System.Drawing.Point(50, 50);
+            this.pnLeftContent.Name = "pnLeftContent";
+            this.pnLeftContent.Size = new System.Drawing.Size(365, 200);
+            this.pnLeftContent.TabIndex = 0;
+            // 
+            // pnRightContent
+            // 
+            this.pnRightContent.Controls.Add(this.panel1);
+            this.pnRightContent.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnRightContent.Location = new System.Drawing.Point(469, 50);
+            this.pnRightContent.Name = "pnRightContent";
+            this.pnRightContent.Size = new System.Drawing.Size(365, 200);
+            this.pnRightContent.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(108)))), ((int)(((byte)(75)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(10);
+            this.panel1.Size = new System.Drawing.Size(365, 54);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(108)))), ((int)(((byte)(75)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(10);
+            this.panel2.Size = new System.Drawing.Size(365, 54);
+            this.panel2.TabIndex = 3;
+            // 
+            // pnButtom
+            // 
+            this.pnButtom.Controls.Add(this.panel6);
+            this.pnButtom.Controls.Add(this.pnButtomContentRight);
+            this.pnButtom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnButtom.Location = new System.Drawing.Point(11, 463);
+            this.pnButtom.Name = "pnButtom";
+            this.pnButtom.Padding = new System.Windows.Forms.Padding(50, 10, 50, 10);
+            this.pnButtom.Size = new System.Drawing.Size(884, 147);
+            this.pnButtom.TabIndex = 2;
+            // 
+            // pnButtomContentRight
+            // 
+            this.pnButtomContentRight.Controls.Add(this.panel5);
+            this.pnButtomContentRight.Controls.Add(this.panel4);
+            this.pnButtomContentRight.Controls.Add(this.panel3);
+            this.pnButtomContentRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnButtomContentRight.Location = new System.Drawing.Point(469, 10);
+            this.pnButtomContentRight.Name = "pnButtomContentRight";
+            this.pnButtomContentRight.Size = new System.Drawing.Size(365, 127);
+            this.pnButtomContentRight.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(175)))), ((int)(((byte)(128)))));
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(365, 42);
+            this.panel3.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(175)))), ((int)(((byte)(128)))));
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 42);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(365, 42);
+            this.panel4.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(175)))), ((int)(((byte)(128)))));
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 84);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(365, 42);
+            this.panel5.TabIndex = 2;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.panel7);
+            this.panel6.Controls.Add(this.panel8);
+            this.panel6.Controls.Add(this.panel9);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel6.Location = new System.Drawing.Point(50, 10);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(365, 127);
+            this.panel6.TabIndex = 1;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(175)))), ((int)(((byte)(128)))));
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel7.Location = new System.Drawing.Point(0, 84);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(365, 42);
+            this.panel7.TabIndex = 2;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(175)))), ((int)(((byte)(128)))));
+            this.panel8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel8.Location = new System.Drawing.Point(0, 42);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(365, 42);
+            this.panel8.TabIndex = 1;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(175)))), ((int)(((byte)(128)))));
+            this.panel9.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel9.Location = new System.Drawing.Point(0, 0);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(365, 42);
+            this.panel9.TabIndex = 0;
             // 
             // ctrlBaseCard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(220)))), ((int)(((byte)(183)))));
+            this.Controls.Add(this.pnButtom);
+            this.Controls.Add(this.pnContecnt);
             this.Controls.Add(this.pnTop);
             this.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold);
             this.Name = "ctrlBaseCard";
             this.Padding = new System.Windows.Forms.Padding(11);
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.Size = new System.Drawing.Size(890, 542);
+            this.Size = new System.Drawing.Size(906, 621);
             this.pnTop.ResumeLayout(false);
             this.pnBaseInfo.ResumeLayout(false);
             this.pnBaseInfo.PerformLayout();
             this.pnFullName.ResumeLayout(false);
             this.pnFullName.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnContecnt.ResumeLayout(false);
+            this.pnLeftContent.ResumeLayout(false);
+            this.pnRightContent.ResumeLayout(false);
+            this.pnButtom.ResumeLayout(false);
+            this.pnButtomContentRight.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -188,5 +352,19 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label lbID;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel pnContecnt;
+        private System.Windows.Forms.Panel pnLeftContent;
+        private System.Windows.Forms.Panel pnRightContent;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel pnButtom;
+        private System.Windows.Forms.Panel pnButtomContentRight;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
     }
 }
