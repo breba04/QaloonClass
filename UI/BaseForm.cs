088a6fa7eaf188c63e1d61e0b28a6bb0c477a17d
abdalbaset <abdallbaset2004@gmail.com>
2026-04-10 19:11:12 +0330

    feat(UI): add BaseForm and global configurations

---
+++ UI/BaseForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class BaseForm : Form
+    {
+        public BaseForm()
+        {
+            InitializeComponent();
+        }
+
+        public string HeaderTitle
+        {
+            get => lbl_Title.Text; 
+            set => lbl_Title.Text = value; 
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Minimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
