73b40916730638443ff6046ddfb662eb27b3ca2e
abdalbaset <abdallbaset2004@gmail.com>
2026-04-24 21:12:37 +0330

    refactor(UI): enhance login logic and validation in frmLogin
    Changes:
    
    Refactoring: The login validation logic is split into separate functions (_IsUserLoginSuccessful, _IsUserActive) to improve code clarity.
    
    Validation: An ErrorProvider is integrated with the Validating event to prevent the submission of null data and provide a better user experience.
    
    BaseForm Compatibility: The form is modified to inherit from BaseForm instead of a standard form, ensuring design consistency.
    
    Enhanced Security and Control: Login is added to verify the account status (IsActive) before allowing login, and the form is closed via DialogResult.OK to enable Program.cs to manage the flow effectively.
    
    Interface Formatting: The form title is made dynamic using clsGlobal.CenterName.

--- UI/Login/frmLogin.Designer.cs
+++ UI/Login/frmLogin.Designer.cs
@@ -38,16 +38,12 @@
             this.txt_UserName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.btn_Close = new System.Windows.Forms.Button();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.btn_Login = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label5 = new System.Windows.Forms.Label();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
-            this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
@@ -64,7 +60,7 @@
             this.ckb_RememberMe.AutoSize = true;
             this.ckb_RememberMe.Cursor = System.Windows.Forms.Cursors.Hand;
             this.ckb_RememberMe.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ckb_RememberMe.Location = new System.Drawing.Point(504, 414);
+            this.ckb_RememberMe.Location = new System.Drawing.Point(502, 379);
             this.ckb_RememberMe.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.ckb_RememberMe.Name = "ckb_RememberMe";
             this.ckb_RememberMe.Size = new System.Drawing.Size(91, 32);
@@ -77,7 +73,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(40)))), ((int)(((byte)(25)))));
-            this.label4.Location = new System.Drawing.Point(499, 320);
+            this.label4.Location = new System.Drawing.Point(497, 285);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(104, 28);
@@ -89,21 +85,22 @@
             this.txt_PassWord.BackColor = System.Drawing.Color.Beige;
             this.txt_PassWord.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txt_PassWord.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_PassWord.Location = new System.Drawing.Point(504, 354);
+            this.txt_PassWord.Location = new System.Drawing.Point(502, 319);
             this.txt_PassWord.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.txt_PassWord.Multiline = true;
             this.txt_PassWord.Name = "txt_PassWord";
             this.txt_PassWord.PasswordChar = '*';
             this.txt_PassWord.Size = new System.Drawing.Size(452, 41);
             this.txt_PassWord.TabIndex = 6;
-            this.txt_PassWord.Tag = "";
+            this.txt_PassWord.Tag = "كلمة المرور";
+            this.txt_PassWord.Validating += new System.ComponentModel.CancelEventHandler(this.txt_Box_Validating);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(40)))), ((int)(((byte)(25)))));
-            this.label3.Location = new System.Drawing.Point(499, 223);
+            this.label3.Location = new System.Drawing.Point(497, 188);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(133, 28);
@@ -115,20 +112,21 @@
             this.txt_UserName.BackColor = System.Drawing.Color.Beige;
             this.txt_UserName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txt_UserName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_UserName.Location = new System.Drawing.Point(504, 257);
+            this.txt_UserName.Location = new System.Drawing.Point(502, 222);
             this.txt_UserName.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.txt_UserName.Multiline = true;
             this.txt_UserName.Name = "txt_UserName";
             this.txt_UserName.Size = new System.Drawing.Size(452, 41);
             this.txt_UserName.TabIndex = 4;
-            this.txt_UserName.Tag = "";
+            this.txt_UserName.Tag = "اسم المستخدم";
+            this.txt_UserName.Validating += new System.ComponentModel.CancelEventHandler(this.txt_Box_Validating);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(499, 170);
+            this.label2.Location = new System.Drawing.Point(497, 135);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(413, 28);
@@ -141,28 +139,12 @@
             this.label1.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(40)))), ((int)(((byte)(25)))));
-            this.label1.Location = new System.Drawing.Point(496, 129);
+            this.label1.Location = new System.Drawing.Point(494, 94);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(180, 38);
             this.label1.TabIndex = 1;
             this.label1.Text = "تسجيل الدخول";
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(40)))), ((int)(((byte)(25)))));
-            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.panel2.Controls.Add(this.btn_Close);
-            this.panel2.Controls.Add(this.label8);
-            this.panel2.Controls.Add(this.label7);
-            this.panel2.Controls.Add(this.label6);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(998, 53);
-            this.panel2.TabIndex = 26;
             // 
             // btn_Close
             // 
@@ -188,32 +170,6 @@
             this.btn_Close.UseVisualStyleBackColor = false;
             this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
             // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.label8.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(214)))), ((int)(((byte)(91)))));
-            this.label8.Location = new System.Drawing.Point(13, 6);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(44, 31);
-            this.label8.TabIndex = 12;
-            this.label8.Text = "نور";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(58, 9);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(163, 28);
-            this.label7.TabIndex = 11;
-            this.label7.Text = "نظام إدارة الحلقات";
-            // 
             // label6
             // 
             this.label6.BackColor = System.Drawing.Color.Transparent;
@@ -238,7 +194,7 @@
             this.btn_Login.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btn_Login.ImageIndex = 0;
             this.btn_Login.ImageList = this.imageList1;
-            this.btn_Login.Location = new System.Drawing.Point(505, 483);
+            this.btn_Login.Location = new System.Drawing.Point(503, 448);
             this.btn_Login.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btn_Login.Name = "btn_Login";
             this.btn_Login.Size = new System.Drawing.Size(451, 61);
@@ -254,11 +210,12 @@
             this.panel1.BackgroundImage = global::UI.Properties.Resources.Login;
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel1.Controls.Add(this.label5);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panel1.Location = new System.Drawing.Point(0, 50);
+            this.panel1.Location = new System.Drawing.Point(0, 54);
             this.panel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(445, 529);
+            this.panel1.Size = new System.Drawing.Size(445, 490);
             this.panel1.TabIndex = 10;
             // 
             // label5
@@ -284,8 +241,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btn_Close;
-            this.ClientSize = new System.Drawing.Size(998, 579);
-            this.Controls.Add(this.panel2);
+            this.ClientSize = new System.Drawing.Size(998, 544);
             this.Controls.Add(this.btn_Login);
             this.Controls.Add(this.ckb_RememberMe);
             this.Controls.Add(this.label4);
@@ -296,16 +252,19 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "frmLogin";
-            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.RightToLeftLayout = true;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmLogin";
             this.Load += new System.EventHandler(this.frmLogin_Load);
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.txt_UserName, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.txt_PassWord, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.ckb_RememberMe, 0);
+            this.Controls.SetChildIndex(this.btn_Login, 0);
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
@@ -325,10 +284,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Button btn_Close;
         private System.Windows.Forms.ErrorProvider errorProvider1;
     }
