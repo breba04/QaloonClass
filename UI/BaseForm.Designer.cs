2c37a5ab7480a208213da62f5cd5c3e7381c2831
abdalbaset <abdallbaset2004@gmail.com>
2026-04-24 20:37:31 +0330

    refactor(UI): standardize BaseForm header and layout components
    For changes:
    
    Adjusting dimensions: Updating the size and location values ​​of components (such as header height) to be more responsive and consistent with the unified design.
    
    Improving the layout: Ensuring that all screens inheriting from BaseForm follow the same structure and use the same components to minimize future designer errors.

--- UI/BaseForm.Designer.cs
+++ UI/BaseForm.Designer.cs
@@ -31,86 +31,51 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BaseForm));
             this.pnl_SectionHeader = new System.Windows.Forms.Panel();
-            this.pnl_SectionTitle = new System.Windows.Forms.Panel();
-            this.lbl_Title = new System.Windows.Forms.Label();
-            this.pnl_SectionCloseAndMinimize = new System.Windows.Forms.Panel();
             this.btn_Minimize = new System.Windows.Forms.Button();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.btn_Close = new System.Windows.Forms.Button();
+            this.pnl_SectionTitle = new System.Windows.Forms.Panel();
+            this.lbl_Title = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.pnl_SectionHeader.SuspendLayout();
             this.pnl_SectionTitle.SuspendLayout();
-            this.pnl_SectionCloseAndMinimize.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnl_SectionHeader
             // 
             this.pnl_SectionHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(47)))), ((int)(((byte)(40)))));
             this.pnl_SectionHeader.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pnl_SectionHeader.Controls.Add(this.btn_Minimize);
+            this.pnl_SectionHeader.Controls.Add(this.btn_Close);
             this.pnl_SectionHeader.Controls.Add(this.pnl_SectionTitle);
-            this.pnl_SectionHeader.Controls.Add(this.pnl_SectionCloseAndMinimize);
             this.pnl_SectionHeader.Controls.Add(this.label6);
             this.pnl_SectionHeader.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnl_SectionHeader.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.pnl_SectionHeader.Location = new System.Drawing.Point(0, 0);
             this.pnl_SectionHeader.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.pnl_SectionHeader.Name = "pnl_SectionHeader";
-            this.pnl_SectionHeader.Size = new System.Drawing.Size(976, 60);
+            this.pnl_SectionHeader.Size = new System.Drawing.Size(976, 54);
             this.pnl_SectionHeader.TabIndex = 28;
-            // 
-            // pnl_SectionTitle
-            // 
-            this.pnl_SectionTitle.BackColor = System.Drawing.Color.Transparent;
-            this.pnl_SectionTitle.Controls.Add(this.lbl_Title);
-            this.pnl_SectionTitle.Dock = System.Windows.Forms.DockStyle.Right;
-            this.pnl_SectionTitle.Location = new System.Drawing.Point(451, 0);
-            this.pnl_SectionTitle.Name = "pnl_SectionTitle";
-            this.pnl_SectionTitle.Size = new System.Drawing.Size(525, 60);
-            this.pnl_SectionTitle.TabIndex = 31;
-            // 
-            // lbl_Title
-            // 
-            this.lbl_Title.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.lbl_Title.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Title.ForeColor = System.Drawing.Color.White;
-            this.lbl_Title.Location = new System.Drawing.Point(21, 9);
-            this.lbl_Title.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lbl_Title.Name = "lbl_Title";
-            this.lbl_Title.Size = new System.Drawing.Size(500, 35);
-            this.lbl_Title.TabIndex = 11;
-            this.lbl_Title.Text = "عنوان";
-            this.lbl_Title.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // pnl_SectionCloseAndMinimize
-            // 
-            this.pnl_SectionCloseAndMinimize.BackColor = System.Drawing.Color.Transparent;
-            this.pnl_SectionCloseAndMinimize.Controls.Add(this.btn_Minimize);
-            this.pnl_SectionCloseAndMinimize.Controls.Add(this.btn_Close);
-            this.pnl_SectionCloseAndMinimize.Location = new System.Drawing.Point(12, 9);
-            this.pnl_SectionCloseAndMinimize.Name = "pnl_SectionCloseAndMinimize";
-            this.pnl_SectionCloseAndMinimize.Size = new System.Drawing.Size(200, 42);
-            this.pnl_SectionCloseAndMinimize.TabIndex = 29;
             // 
             // btn_Minimize
             // 
-            this.btn_Minimize.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.btn_Minimize.BackColor = System.Drawing.Color.Transparent;
             this.btn_Minimize.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_Minimize.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_Minimize.Dock = System.Windows.Forms.DockStyle.Left;
             this.btn_Minimize.FlatAppearance.BorderSize = 0;
             this.btn_Minimize.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(229)))), ((int)(((byte)(230)))), ((int)(((byte)(229)))));
             this.btn_Minimize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(229)))), ((int)(((byte)(230)))), ((int)(((byte)(229)))));
             this.btn_Minimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_Minimize.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_Minimize.ForeColor = System.Drawing.Color.White;
-            this.btn_Minimize.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btn_Minimize.ImageIndex = 1;
             this.btn_Minimize.ImageList = this.imageList1;
-            this.btn_Minimize.Location = new System.Drawing.Point(55, 5);
+            this.btn_Minimize.Location = new System.Drawing.Point(55, 0);
             this.btn_Minimize.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btn_Minimize.Name = "btn_Minimize";
-            this.btn_Minimize.Size = new System.Drawing.Size(43, 29);
-            this.btn_Minimize.TabIndex = 30;
+            this.btn_Minimize.Size = new System.Drawing.Size(55, 54);
+            this.btn_Minimize.TabIndex = 32;
             this.btn_Minimize.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btn_Minimize.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btn_Minimize.UseVisualStyleBackColor = false;
@@ -121,32 +86,55 @@
             this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
             this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
             this.imageList1.Images.SetKeyName(0, "close2.png");
-            this.imageList1.Images.SetKeyName(1, "Ic_Minimize.png");
+            this.imageList1.Images.SetKeyName(1, "minimize.png");
             // 
             // btn_Close
             // 
-            this.btn_Close.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.btn_Close.BackColor = System.Drawing.Color.Transparent;
             this.btn_Close.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_Close.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_Close.Dock = System.Windows.Forms.DockStyle.Left;
             this.btn_Close.FlatAppearance.BorderSize = 0;
             this.btn_Close.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(229)))), ((int)(((byte)(230)))), ((int)(((byte)(229)))));
             this.btn_Close.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(229)))), ((int)(((byte)(230)))), ((int)(((byte)(229)))));
             this.btn_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_Close.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_Close.ForeColor = System.Drawing.Color.White;
-            this.btn_Close.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btn_Close.ImageIndex = 0;
             this.btn_Close.ImageList = this.imageList1;
-            this.btn_Close.Location = new System.Drawing.Point(4, 5);
+            this.btn_Close.Location = new System.Drawing.Point(0, 0);
             this.btn_Close.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btn_Close.Name = "btn_Close";
-            this.btn_Close.Size = new System.Drawing.Size(43, 29);
-            this.btn_Close.TabIndex = 29;
+            this.btn_Close.Size = new System.Drawing.Size(55, 54);
+            this.btn_Close.TabIndex = 30;
             this.btn_Close.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btn_Close.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btn_Close.UseVisualStyleBackColor = false;
             this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // pnl_SectionTitle
+            // 
+            this.pnl_SectionTitle.BackColor = System.Drawing.Color.Transparent;
+            this.pnl_SectionTitle.Controls.Add(this.lbl_Title);
+            this.pnl_SectionTitle.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnl_SectionTitle.Location = new System.Drawing.Point(451, 0);
+            this.pnl_SectionTitle.Name = "pnl_SectionTitle";
+            this.pnl_SectionTitle.Size = new System.Drawing.Size(525, 54);
+            this.pnl_SectionTitle.TabIndex = 31;
+            // 
+            // lbl_Title
+            // 
+            this.lbl_Title.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.lbl_Title.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbl_Title.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Title.ForeColor = System.Drawing.Color.White;
+            this.lbl_Title.Location = new System.Drawing.Point(0, 0);
+            this.lbl_Title.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Title.Name = "lbl_Title";
+            this.lbl_Title.Size = new System.Drawing.Size(525, 42);
+            this.lbl_Title.TabIndex = 11;
+            this.lbl_Title.Text = "عنوان";
+            this.lbl_Title.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label6
             // 
@@ -175,7 +163,6 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.pnl_SectionHeader.ResumeLayout(false);
             this.pnl_SectionTitle.ResumeLayout(false);
-            this.pnl_SectionCloseAndMinimize.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -184,11 +171,10 @@
 
         private System.Windows.Forms.Panel pnl_SectionHeader;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Button btn_Close;
         private System.Windows.Forms.ImageList imageList1;
-        private System.Windows.Forms.Panel pnl_SectionCloseAndMinimize;
-        private System.Windows.Forms.Button btn_Minimize;
         private System.Windows.Forms.Panel pnl_SectionTitle;
         public System.Windows.Forms.Label lbl_Title;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Button btn_Minimize;
     }
 }
