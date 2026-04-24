735ea8f684278161b91f56d176a529c67fd7cf05
abdalbaset <abdallbaset2004@gmail.com>
2026-04-24 21:01:58 +0330

    feat: implement local credential storage using Windows Registry
    Changes:
    
    Implement the StoreUserNameAndPasswordInRegistry function to provide the "Remember Me" feature by saving login credentials to a local Windows Registry.
    
    Define the KeyPath for the Noor Quran Memorization Center in the Registry to ensure data organization.

--- UI/GlobalClasses/Global.cs
+++ UI/GlobalClasses/Global.cs
@@ -1,13 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Microsoft.Win32;
+using System;
 namespace UI.GlobalClasses
 {
     public class clsGlobal
     {
         static public string CenterName = "نور";
+
+        static public string KeyPath = @"HKEY_CURRENT_USER\SOFTWARE\ABDULBASITAndIbrahim\TahfezCenter_System";
+
+       static public  void StoreUserNameAndPassWordInRegistry(string UserName ,string Password)
+        {
+            try
+            {
+                 Registry.SetValue(clsGlobal.KeyPath, "UserName", UserName);
+                 Registry.SetValue(clsGlobal.KeyPath, "Password", Password);
+            }
+            catch (Exception ex)
+            {
+                // Errors will be recorded in the LOG file later.
+
+            }
+        }
     }
 }
