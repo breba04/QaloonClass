58e8b6c74a5e11c00be50fe740c8b1bbde41c24d
abdalbaset <abdallbaset2004@gmail.com>
2026-04-24 21:07:20 +0330

    refactor(UI\Users): optimize Login flow in clsCurrentUser
    Changes:
    
    Improved Login Login Login: The Login function has been modified to operate directly on the static CurrentUser property instead of accepting clsUsers as a parameter, simplifying function calls in user interfaces.
    
    Remember Me Integration: The Registry function (clsGlobal.Store...) has been integrated into the login flow to automatically implement the "Remember Me" feature upon successful login.
    
    Improved Code Efficiency: Unnecessary operators have been removed, and the loginDetails call structure has been updated to rely on data already stored in CurrentUser.

--- UI/Users/clsCurrentUser.cs
+++ UI/Users/clsCurrentUser.cs
@@ -5,23 +5,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UI.GlobalClasses;
 
 namespace UI.Users
 {
     public class clsCurrentUser
     {
         static public clsUsers CurrentUser { get; set; }
-        static public void Login(clsUsers user,bool isRemember,string Notes = null)
+        static public void Login(bool isRemember,string Notes = null)
         {
-            CurrentUser = user;
+            if(CurrentUser == null )
+            {
+                return;
+            }
+
             clsEntityclsLoginDetails loginDetails = new clsEntityclsLoginDetails()
             {
-                UserID = user.UserData.UserID,
+                UserID = CurrentUser.UserID,
                 LoginDate = DateTime.Now,
                 IsRemember = isRemember,
                 Notes = Notes
             };
-            clsLoginDetails.AddLoginDetails(loginDetails);
+
+            if (isRemember)
+                clsGlobal.StoreUserNameAndPassWordInRegistry(CurrentUser.UserName,CurrentUser.Password);
+
+                clsLoginDetails.AddLoginDetails(loginDetails);
         }
         static public void LogOut()
         {
