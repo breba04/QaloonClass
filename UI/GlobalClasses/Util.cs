c9962d503ffc40eec369d1b18ce7f283aca94396
abdalbaset <abdallbaset2004@gmail.com>
2026-05-17 11:34:52 +0330

    feat: add clsUtil helper class for shared UI styling logic
    Description:
    
    - Created a new utility class 'clsUtil' under global classes to       encapsulate repetitive UI logic and promote code reuse (DRY principle).
    
    - Implemented 'InitializeGridViewStyle' method to apply a  unified, professional theme across all DataGridViews (Dark Green #002819 headers, centered text, and custom selection color #2E4F3F).
    
    - Implemented 'ConfigureColumn' method to simplify programming grid columns dynamically by setting headers and custom widths in a clean, one-line execution.

---
+++ UI/GlobalClasses/Util.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI.GlobalClasses
+{
+    public class clsUtil
+    {
+
+        static public void InitializeGridViewStyle(DataGridView dgv)
+        {
+            dgv.EnableHeadersVisualStyles = false;
+
+            using (Font headerFont = new Font("Segoe UI", 11, FontStyle.Bold))
+            {
+                dgv.ColumnHeadersDefaultCellStyle.Font = headerFont;
+                dgv.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+                dgv.ColumnHeadersDefaultCellStyle.BackColor = ColorTranslator.FromHtml("#002819");
+                dgv.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+
+                dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+                dgv.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                dgv.DefaultCellStyle.SelectionBackColor = ColorTranslator.FromHtml("#2E4F3F");
+                dgv.RowTemplate.Height = 35;
+
+                dgv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+                dgv.ColumnHeadersHeight = 50;
+
+                dgv.GridColor = Color.LightGray;
+            }
+       
+        }
+        static public void ConfigureColumn(DataGridViewColumn column, string headerText, int width)
+        {
+            if (column != null)
+            {
+                column.HeaderText = headerText;
+                column.Width = width;
+            }
+        }
+    }
+}
