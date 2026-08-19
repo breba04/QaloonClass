3f2e747de9180b4636952ca4f20cc665b032bfa9
abdalbaset <abdallbaset2004@gmail.com>
2026-08-19 15:42:39 +0330

    feat(core): add AppEvents class for global event handling
    - Implemented the Publisher/Subscriber pattern using an `AppEvents` class (Event Bus).
    
    - Added `StudentAdded` event to decouple form interactions.

---
+++ UI/GlobalClasses/AppEvents.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.GlobalClasses
+{
+    public class clsAppEvents
+    {
+        public static event EventHandler StudentAdded;
+        public static void OnStudentAdded()
+        {
+            StudentAdded?.Invoke(null, EventArgs.Empty);
+        }
+    }
+}
